f284e839cf5c5cabb92bb7d1f67d6d9c42d3028c
Elon421 <Lakshyachanna8@gmail.com>
2024-04-26 14:23:59 +0200

    Add project files.

---
+++ ConsoleApp1/Tafel.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp1;
+
+public class Tafel
+{
+    public int Tafel_Id { get; set; }
+    public int Max_Aantal_Personen { get; set; }
+    public override string ToString() => $"ID = {Tafel_Id}, Maximum personen = {Max_Aantal_Personen}";
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "server=127.0.0.1;user=root;password=;database=test";
+
+        MySqlConnection connection = new MySqlConnection(connectionString);
+
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Connection successful!");
+
+     
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+}
+
+
+
+
+
